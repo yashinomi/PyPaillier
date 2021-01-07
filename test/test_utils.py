7c9a8055327be832f696e397bb7a333496cccbf1
yashinomi <by.yashinomi@gmail.com>
2021-01-07 22:26:19 +0900

    remove template and add test for lcm, and mod inverse

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,9 +1,15 @@
 import unittest
 
+from pypaillier import utils
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestUtils(unittest.TestCase):
+    def test_universal_mod_inverse(self):
+        self.assertEqual(utils.mod_inv(3, 11), 4)
+
+    def test_lcm(self):
+        self.assertEqual(utils.lcm(3, 4), 12)
+        self.assertEqual(utils.lcm(3, 6), 6)
+        self.assertEqual(utils.lcm(28, 12), 84)
 
 
 if __name__ == '__main__':

bd1bec8c45e95302e3259b3e708d4a02eab0ba86
yashinomi <by.yashinomi@gmail.com>
2021-01-08 13:42:07 +0900

    fix error caused by less arguments

--- pypaillier/encoded_message.py
+++ pypaillier/encoded_message.py
@@ -1,8 +1,8 @@
-from .publickey import PublicKey
-
 class EncodedMessage:
-    def __init__(self, message, public_key: PublicKey):
+    def __init__(self, message, public_key):
         self.m = message
+        self.pk = public_key
+
         self.n = public_key.n
         self.n2 = public_key.n2
 
@@ -10,13 +10,13 @@ class EncodedMessage:
         if not isinstance(other, EncodedMessage):
             raise TypeError("Addition is only supported between EncodedMessage")
 
-        return EncodedMessage(self.m * other.m % self.n2)
+        return EncodedMessage(self.m * other.m % self.n2, self.pk)
 
     def __mul__(self, other) -> "EncodedMessage":
         if type(other) is not int:
             raise TypeError("Multiplication is only supported with integer")
 
-        return EncodedMessage(self.m ** other % self.n2)
+        return EncodedMessage(pow(self.m, other, self.n2), self.pk)
 
     def __int__(self) -> int:
         return self.m

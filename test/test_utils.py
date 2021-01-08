c1a330f0a40180f3cf8940a093b1e84eeb6073d9
yashinomi <by.yashinomi@gmail.com>
2021-01-08 10:50:07 +0900

    fix arguments

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,12 +4,12 @@ from pypaillier import utils
 
 class TestUtils(unittest.TestCase):
     def test_universal_mod_inverse(self):
-        self.assertEqual(utils.mod_inv(3, 11), 4)
+        self.assertEqual(4, utils.mod_inv(3, 11))
 
     def test_lcm(self):
-        self.assertEqual(utils.lcm(3, 4), 12)
-        self.assertEqual(utils.lcm(3, 6), 6)
-        self.assertEqual(utils.lcm(28, 12), 84)
+        self.assertEqual(12, utils.lcm(3, 4))
+        self.assertEqual(6, utils.lcm(3, 6))
+        self.assertEqual(84, utils.lcm(28, 12))
 
 
 if __name__ == '__main__':
